6648f787ba530a1e05ad334abe1c583b8c302bc1
olafpapa <s.iwatsuru@gmail.com>
2019-08-19 20:42:24 +0900

    例文が表示されたら画面サイズを超得る場合があり表示が欠けるので、SingleChildScrollViewを使う。

--- lib/components/quiz.dart
+++ lib/components/quiz.dart
@@ -10,35 +10,37 @@ class Quiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<QuestionsModel>(builder: (context, question, _) {
-      return Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Question(),
-            ...(question.currentAnswers).map((Word answer) {
-              return Column(
-                children: <Widget>[
-                  AnswerButton(answer),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 25,
-                  )
-                ],
-              );
-            }).toList(),
-            question.isSelectAnswer()
-                ? Sentence(question.correctWord)
-                : SizedBox.shrink(),
-            question.isSelectAnswer()
-                ? FlatButton(
-                    child: question.isLastQuestion()
-                        ? Text('結果を見る')
-                        : Text('${question.questionIndex + 2}問目へ'),
-                    onPressed: () => question.nextIndex(),
-                    textColor: Colors.blue,
-                  )
-                : SizedBox.shrink(),
-          ],
+      return SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Question(),
+              ...(question.currentAnswers).map((Word answer) {
+                return Column(
+                  children: <Widget>[
+                    AnswerButton(answer),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 25,
+                    )
+                  ],
+                );
+              }).toList(),
+              question.isSelectAnswer()
+                  ? Sentence(question.correctWord)
+                  : SizedBox.shrink(),
+              question.isSelectAnswer()
+                  ? FlatButton(
+                      child: question.isLastQuestion()
+                          ? Text('結果を見る')
+                          : Text('${question.questionIndex + 2}問目へ'),
+                      onPressed: () => question.nextIndex(),
+                      textColor: Colors.blue,
+                    )
+                  : SizedBox.shrink(),
+            ],
+          ),
         ),
       );
     });
