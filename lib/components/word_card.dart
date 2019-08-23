87a28d15aa7e7bc5e051f2fe36e3656128abcb9d
olafpapa <s.iwatsuru@gmail.com>
2019-08-23 21:39:43 +0900

    品詞を単語カードに表示するようにした。

--- lib/components/word_card.dart
+++ lib/components/word_card.dart
@@ -28,10 +28,36 @@ class WordCard extends StatelessWidget {
             Container(
               width: double.infinity,
               padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-              child: Text(
-                word.english,
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              child: Row(
+                children: <Widget>[
+                  word.wordClass != null
+                      ? Container(
+                          margin: const EdgeInsets.all(0),
+                          padding: const EdgeInsets.all(1.0),
+                          decoration: new BoxDecoration(
+                            border: new Border.all(color: Colors.black54),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                          ),
+                          child: Text(
+                            word.wordClass[0],
+                            style:
+                                TextStyle(fontSize: 12, color: Colors.black54),
+                          ),
+                        )
+                      : SizedBox.shrink(),
+                  Container(
+                    padding: word.wordClass != null
+                        ? EdgeInsets.only(left: 10)
+                        : EdgeInsets.all(0),
+                    child: Text(
+                      word.english,
+                      textAlign: TextAlign.left,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
